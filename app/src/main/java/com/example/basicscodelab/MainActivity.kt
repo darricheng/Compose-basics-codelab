743787e3f0f17d3b3bdba95103b43df510ca3e3d
Darric Heng <darricheng@gmail.com>
2023-12-26 17:36:26 +0800

    learn about state

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +47,8 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded: MutableState<Boolean> = remember { mutableStateOf(false) }
+    val extraPad = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -51,14 +56,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
                     .weight(1f)
+                    .padding(bottom = extraPad)
             ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text("Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                val text = if (expanded.value) {
+                    "Show less"
+                } else {
+                    "Show more"
+                }
+                Text(text)
             }
         }
     }
